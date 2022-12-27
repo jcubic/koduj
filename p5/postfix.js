bdd9b4b336b1adf634f83a56417aaaea2db22f8b
Jakub Jankiewicz <jcubic@onet.pl>
2022-12-27 12:55:03 +0100

    fix accessing module variables from console

--- p5/postfix.js
+++ p5/postfix.js
@@ -14,3 +14,7 @@
         } catch(e) {}
     });
 })();
+// ref: https://stackoverflow.com/q/67322922/387194
+var __EVAL = window.__EVAL = (s) => {
+    return eval(`void (__EVAL = ${__EVAL.toString()}); ${s}`);
+};
